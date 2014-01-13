80e1a0b1c6f35fca5f7957fe372be6b938797ac5
alexharris <alexharris6@gmail.com>
2014-01-12 21:56:17 -0800

    fix some stuff; redo a bunch of js so that color picker is in modal

--- frontend/app/scripts/main.js
+++ frontend/app/scripts/main.js
@@ -9,6 +9,29 @@ var colors = ['fbb85d','ed257b']; //create array for building gradient
 var currentColorStopValues = [gradientSize, 0]; //initial values for handle position
 
 
+// convert RGB to hex
+var bg;
+$.cssHooks.backgroundColor = {
+	get: function(elem) {
+		if (elem.currentStyle) {
+			bg = elem.currentStyle.backgroundColor;
+		} else if (window.getComputedStyle) {
+			bg = document.defaultView.getComputedStyle(elem,null).getPropertyValue('background-color');
+		}
+		if (bg.search('rgb') === -1) {
+			return bg;
+		} else {
+			bg = bg.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+
+			return '#' + hex(bg[1]) + hex(bg[2]) + hex(bg[3]);
+		}
+	}
+};
+
+function hex(x) {
+	return ('0' + parseInt(x).toString(16)).slice(-2);
+}
+
 //add new handle to slider
 function addColorStopHandle(){
 	$('.add-new').click(function(){
@@ -39,21 +62,20 @@ function initSlider(){
 	});
 }
 
-function addNewColor() {
-	$('.ui-slider-handle').each(function(i){
-		console.log('# of sliders: ' + i);
-		if($(this).children('.minicolors').length > 0){
-			var offset = $(this).offset();
-		} else {
-			var input = document.createElement('input');
-			input.type = 'hidden';
-			input.value= colors[i];
-			input.className = 'pick-a-color'; // set the CSS class
-			$(this).append(input);
-			callPicker(input);
-			updateColorArray(input);
-		}
+function addNewColor(){
+	$('.ui-slider-handle').click(function(){
+		$('.ui-slider-handle').removeClass('currentHandle');
+		$('.modal-picker').modal();
+		$(this).addClass('current-handle');
 	});
+
+	// $('.ui-slider-handle').each(function(i){
+	// 	if($(this).children('.minicolors').length > 0){
+	// 		var offset = $(this).offset();
+	// 	} else {
+	// 		updateColorArray(input);
+	// 	}
+	// });
 }
 
 function updateColorStopArray() {
@@ -64,29 +86,48 @@ function updateColorStopArray() {
 		if (offsetTop < 0) {
 			offsetTop = 0;
 		}
-		console.log('offset top: ' + offsetTop);
 		currentColorStopValues.push(offsetTop);
 	});
 }
 
 //calls pick-a-color on an input
-function callPicker(input){
-	$(input).minicolors({
+function callPicker(){
+
+	var modalSwatch = $('.modal-swatch');
+
+	$('.minicolors-input').minicolors({
 		theme: 'digitalSunset',
-		defaultValue: '#000000'
+		defaultValue: '#000000',
+		inline: true,
+		change: function(hex, opacity) {
+			modalSwatch.css('background-color', hex);
+		}
+	});
+}
+
+function modalColorOk(){
+	$('.modal-color-ok').click(function(){
+		var swatchColor = $('.modal-swatch').css('background-color');
+		$('.ui-slider-handle.current-handle').css('background-color', swatchColor);
+		$('.ui-slider-handle.current-handle').removeClass('current-handle');
+		updateColorArray();
 	});
 }
 
 // build array of colors from pick-a-color inputs
-function updateColorArray(input){
-	$(input).change(function() {
-		colors = [];
-		$('.pick-a-color').each(function(){
-			colors.push(this.value);
-		});
-		createGradient(colors);
-
+function updateColorArray(){
+	colors = [];
+	var cssBackgroundColor;
+	console.log('initial array: ' + colors);
+	$('.ui-slider-handle').each(function(i, val){
+		console.log(i);
+		var cssBackgroundColor = $(this).css('backgroundColor');
+		console.log('this handle: ' + cssBackgroundColor);
+		colors.push(cssBackgroundColor);
+		console.log('after push: ' + colors);
 	});
+	
+	createGradient(colors);
 }
 
 function createGradient(colors){
@@ -100,9 +141,9 @@ function createGradient(colors){
 	}
 	var i;
 	for (i = 0; i < colors.length; ++i) {
-		console.log(i);
-		console.log('colors: ' + colors);
-		console.log('colorstop values: ' + currentColorStopValues);
+		// console.log(i);
+		// console.log('colors: ' + colors);
+		// console.log('colorstop values: ' + currentColorStopValues);
 		myGradient.addColorStop((currentColorStopValues[i] / gradientSize), colors[i]);
 	}
 	gradContext.fillStyle = myGradient;
@@ -112,13 +153,13 @@ function createGradient(colors){
 function saveCanvas(){
 	$('.save-button').click(function(){
 		var canvas = document.getElementById('gradient');
-		var img    = canvas.toDataURL('image/png');
+		var img = canvas.toDataURL('image/png');
 		$('.modal-save .modal-body .img').replaceWith('<div class="img"></div><img src="'+img+'"/></div>');
 		$('.modal-save').modal();
 
 		//save to form
-		gradientForm = document.getElementById('form-gradient');
-		gradientForm.value = img;
+		// gradientForm = document.getElementById('form-gradient');
+		// gradientForm.value = img;
 	});
 }
 
@@ -129,13 +170,30 @@ function startGradient(){
 	});
 }
 
+function questionBox(){
+	$('.question').click(function(){
+		$('.question-box').slideToggle();
+	});
+	$('.question-box-close').click(function(){
+		$('.question-box').slideToggle();
+	});
+}
 
-//Load methods on document ready
-$(document).ready(function(){
 
+function intialHandleLoad(){
 	// create initial handles
 	createColorStopHandle(0);
 	createColorStopHandle(gradientSize);
+
+	//color them
+	$('.ui-slider-handle').each(function(i, val){
+		$(this).css('background', '#' + colors[i]);
+	});
+}
+
+
+//Load methods on document ready
+$(document).ready(function(){
 
 	// click to add new handle
 	addColorStopHandle();
@@ -143,10 +201,20 @@ $(document).ready(function(){
 	//load initial gradient
 	createGradient(colors);
 
+	//load and color intial handles
+	intialHandleLoad();
+
+	//call picker 
+	callPicker();
+
+	modalColorOk();
+
 	// print canvas to jpg
 	saveCanvas();
 
 	//unhide gradient maker
 	startGradient();
+
+	questionBox();
 
 });
